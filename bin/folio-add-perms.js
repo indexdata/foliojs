726820e8af3f376873bedca893ae8805b24a78c0
Mike Taylor <mike@indexdata.com>
2023-06-29 18:07:19 +0100

    Move unused code out of folio-update-md.js

---
+++ bin/folio-add-perms.js
@@ -0,0 +1,9 @@
+// XXX not yet supported
+
+/*
+if (process.env.UPDATE_USER_PERMS) {
+  const session2 = await service.login('harvester', 'harvester_admin', 'xyz123');
+  const perms = md.permissionNames();
+  await session2.addPermsToUser('mike', perms);
+}
+*/
--- bin/folio-update-md.js
+++ bin/folio-update-md.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-// In development, run with: yarn exec ./folio-update-md.js
-
 import Folio from '../lib/index.js';
 
 const password = process.env.FOLIO_PASSWORD;
@@ -13,14 +11,8 @@ if (!password) {
 const md = Folio.parseModuleDescriptor('package', '../../../stripes/ui-harvester-admin/package.json');
 const id = md.getId();
 const service = Folio.service('https://harvester-dev-okapi.folio-dev.indexdata.com');
-const session1 = await service.login('supertenant', 'okapi_admin', password);
-await session1.disable('harvester', id);
+const session = await service.login('supertenant', 'okapi_admin', password);
+await session.disable('harvester', id);
 md.incrementVersion();
-await session1.postModule(md);
-await session1.enable('harvester', md.getId());
-
-if (process.env.UPDATE_USER_PERMS) {
-  const session2 = await service.login('harvester', 'harvester_admin', 'xyz123');
-  const perms = md.permissionNames();
-  await session2.addPermsToUser('mike', perms);
-}
+await session.postModule(md);
+await session.enable('harvester', md.getId());
