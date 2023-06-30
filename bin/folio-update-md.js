7ebe09bc38d46dad9a54724065102f5097552429
Mike Taylor <mike@indexdata.com>
2023-06-30 10:24:23 +0100

    Generalize folio-update-md.js

--- bin/folio-update-md.js
+++ bin/folio-update-md.js
@@ -2,17 +2,27 @@
 
 import Folio from '../lib/index.js';
 
-const password = process.env.FOLIO_PASSWORD;
-if (!password) {
-  console.error(process.argv[1], 'cannot run without $FOLIO_PASSWORD');
+if (process.argv.length !== 5) {
+  console.error('Usage:', process.argv[1], '<targetTenant> [md|package] <descriptor>');
   process.exit(1);
 }
+const targetTenant = process.argv[2];
+const mdType = process.argv[3];
+const mdFilename = process.argv[4];
 
-const md = Folio.parseModuleDescriptor('package', '../../../stripes/ui-harvester-admin/package.json');
-const id = md.getId();
-const service = Folio.service('https://harvester-dev-okapi.folio-dev.indexdata.com');
-const session = await service.login('supertenant', 'okapi_admin', password);
-await session.disable('harvester', id);
+const URL = process.env.OKAPI_URL || 'https://harvester-dev-okapi.folio-dev.indexdata.com';
+const TENANT = process.env.OKAPI_TENANT || 'supertenant';
+const USER = process.env.OKAPI_USER || 'okapi_admin';
+const PW = process.env.OKAPI_PW;
+if (!PW) {
+  console.error(process.argv[1], 'cannot run without $OKAPI_PW');
+  process.exit(2);
+}
+
+const md = Folio.parseModuleDescriptor(mdType, mdFilename);
+const service = Folio.service(URL);
+const session = await service.login(TENANT, USER, PW);
+await session.disable(targetTenant, md.getId());
 md.incrementVersion();
 await session.postModule(md);
-await session.enable('harvester', md.getId());
+await session.enable(targetTenant, md.getId());
